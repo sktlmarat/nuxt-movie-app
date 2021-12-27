486515a9dab57b503fb7afaa9cdcef2d8c47825e
Turlubayev Marat <marat.turlubayev@nu.edu.kz>
2021-12-27 15:17:58 +0600

    added api calls

---
+++ layouts/default.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class='app'>
+    <Nuxt />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'default'
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/movies/_movieid.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: '_movieid'
+}
+</script>
+
+<style scoped>
+
+</style>
